93825ee047284cdc0e1cf594bc1e2ba36deaed6d
Johnny Pack <packjc@etsu.edu>
2021-03-10 01:49:14 -0500

    .

--- main.py
+++ main.py
@@ -6,27 +6,54 @@ import praw
 import datetime as dt
 import re
 
+
+
+import matplotlib.pyplot as plt
+
+
+server = "www.nasdaqtrader.com"
+directory = "SymbolDirectory"
+filename = "nasdaqlisted.txt"
+
+#from ftplib import FTP
+#ftp = FTP(server) #Set server address
+#ftp.login()  # Connect to server
+#ftp.cwd(directory) # Move to the desired folder in server
+#ftp.retrbinary('RETR ' + filename,open(filename, 'wb').write) # Download file from server
+#ftp.close() # Close connection
+
+NYSE_Tokens = []
+
+#get file object
+f = open("nasdaqlisted.txt", "r")
+
+while(True):
+    line = f.readline()
+    NYSE_Tokens.append(line.split('|')[0])
+    if not line:
+        break
+#close file
+f.close
+
+
+
 blacklist_words = [
-      "YOLO", "TOS", "CEO", "CFO", "CTO", "DD", "BTFD", "WSB", "OK", "RH",
-      "KYS", "FD", "TYS", "US", "USA", "IT", "ATH", "RIP", "BMW", "GDP",
-      "OTM", "ATM", "ITM", "IMO", "LOL", "DOJ", "BE", "PR", "PC", "ICE",
-      "TYS", "ISIS", "PRAY", "PT", "FBI", "SEC", "GOD", "NOT", "POS", "COD",
-      "AYYMD", "FOMO", "TL;DR", "EDIT", "STILL", "LGMA", "WTF", "RAW", "PM",
-      "LMAO", "LMFAO", "ROFL", "EZ", "RED", "BEZOS", "TICK", "IS", "DOW"
-      "AM", "PM", "LPT", "GOAT", "FL", "CA", "IL", "PDFUA", "MACD", "HQ",
-      "OP", "DJIA", "PS", "AH", "TL", "DR", "JAN", "FEB", "JUL", "AUG",
-      "SEP", "SEPT", "OCT", "NOV", "DEC", "FDA", "IV", "ER", "IPO", "RISE"
-      "IPA", "URL", "MILF", "BUT", "SSN", "FIFA", "USD", "CPU", "AT",
-      "GG", "ELON" , "DFV" , "A" , "I" , "THE" , "TO" , "YOU" , "FOR" , "AND",
-      "MY", "Y", "WE", "THIS", "JPOW", "NO", "DIP", "IN", "ALL", "HOLD", "BUY",
-      "EOD", "YES", "TD", "FUCK"
+      "YOLO",  "CFO", "CTO", "DD", "RH", "USA", "IT", "ATH", "GDP",
+      "ITM", "IMO", "BE", "ICE", "PT", "LOVE", "EDIT", "PM", "DOW",
+      "AM", "PM", "GOAT", "FL", "CA", "IL", "PS", "AH", "TL", "DR",
+      "JAN", "FEB", "JUL", "AUG", "OCT", "NOV", "FDA", "IV", "ER",
+      "IPO", "RISE", "IPA", "URL", "USD", "AT", "A" , "I" , "THE" ,
+      "TO" , "YOU" , "FOR" , "AND", "Y", "WE", "NO", "DIP", "IN",
+      "ALL", "HOLD", "BUY", "EOD", "TD", "MOON", "", "ARE", "NOW",
+      "UP", "BOYS", "GO", "OF", "ON", "IF", "AM", "ME", "HAS", "CAN",
+      "APR", "TECH", "RUN", "GOOD", "OLD", "LIFE", "BBQ"
    ]
    
 def get_tokens(posts, subredditName):
     redditObject = praw.Reddit(client_id = 'qZwrzSmQ6F-7Kg',
                          client_secret = '6bxmPxFlzpVT4SYUWXH6FdT1jbJrMw',
                          username = 'DabCam',
-                         password = '*',
+                         password = 'dabcam21',
                          user_agent= 'SubReddit Sraper')
     selectedSubreddit = redditObject.subreddit(subredditName)
     #Sorts by new and pulls the last (n) posts of (subreddit)
@@ -56,14 +83,16 @@ commentList.sort(key = lambda x:x[1])
 #Think of this as going through every word and adding valid tokens
 #Over a large enough amount of data, this is very accurate for picking up the most talked about 'tokens'
 tickerList = []
+
+
 for i in commentList:
     tickers = re.findall('[A-Z]{1,4}', str(i[0])) 
     if len(tickers) > 0:
         for j in tickers:
-            #clean_tick = str(j[1:])
-            #tickerlist.append(clean_tick.upper())
-            if j and j not in blacklist_words:
-                tickerList.append(j)    
+            if j and j in NYSE_Tokens and j not in blacklist_words:
+                tickerList.append(j)
+
+                
 #Removes duplicate tokens             
 tickerList = set(tickerList)
 #Logs on, scrapes comments, creates ticker objects
@@ -74,9 +103,14 @@ graph_list = []
 for obj in final_list:
     graph_list.append([obj.ticker,obj.count,obj.avg_sent])
     
+    
 df = pandas.DataFrame(graph_list, columns = ['Ticker', 'Count', 'Avg_Sentiment']) #creates a dataframe from the graph_list
 df = df.sort_values(by = 'Count', ascending = False)#Sorts it by count
 df = df.head(10) #Gives us the top 10 rows
+
+print("test")
+
+
 fig = px.bar(df, x = df.Ticker, y = df.Count, color = df.Avg_Sentiment) #creates bar graph
 fig.update_layout(
     title={
@@ -85,3 +119,36 @@ fig.update_layout(
         'xanchor': 'center',
         'yanchor': 'top'})
 fig.show() #initiates graph
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
